8348cdd4e37f9801624a0a59eccef69de035c01d
BanksTools <93403273+BanksTools@users.noreply.github.com>
2021-10-29 20:52:55 +0200

    Add files via upload

---
+++ joiner.py
@@ -0,0 +1,29 @@
+import requests
+
+
+
+print("  ____              _               _       _  ")               
+print(" |  _ \            | |             | |     (_) ")               
+print(" | |_) | __ _ _ __ | | _____       | | ___  _ _ __   ___ _ __ ")
+print(" |  _ < / _` | '_ \| |/ / __|  _   | |/ _ \| | '_ \ / _ \ '__|")
+print(" | |_) | (_| | | | |   <\__ \ | |__| | (_) | | | | |  __/ |   ")
+print(" |____/ \__,_|_| |_|_|\_\___/  \____/ \___/|_|_| |_|\___|_|   ")
+                                                              
+print("                                      Made by Banks#6485")
+
+link = input('Discord Invite Link: ')
+if len(link) > 6:
+    link = link[19:]
+apilink = "https://discordapp.com/api/v6/invite/" + str(link)
+
+print (link)
+
+with open('tokens.txt','r') as handle:
+        tokens = handle.readlines()
+        for x in tokens:
+            token = x.rstrip()
+            headers={
+                'Authorization': token
+                }
+            requests.post(apilink, headers=headers)
+        print ("All valid tokens have joined!")
